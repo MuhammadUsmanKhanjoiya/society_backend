4aaa09faefdf94e84f8fb64f9b520d3831b52b19
MuhammadUsmanKhanjoiya <usmanjoiya981@gmail.com>
2025-07-25 05:12:24 -0700

    .

--- routes/staffRoutes.js
+++ routes/staffRoutes.js
@@ -6,7 +6,7 @@ import authorizRole from '../middleware/roleMiddleware.js';
 const router = express.Router();
 
 router.post('/',verifyToken, authorizRole("admin"), createStaff);
-router.get('/',verifyToken, getAllStaff);
+router.get('/',verifyToken, authorizRole("admin"), getAllStaff);
 router.delete('/:id',verifyToken , authorizRole("admin") , deleteStaff);
 
 export default router;
